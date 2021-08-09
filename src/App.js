335c9cbcf7f7ba28dec12b71302b0995b56e898b
Bryan's Mac <bryanpgomes@gmail.com>
2021-08-09 02:21:21 -0400

    Establishing base for website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+const admins = [];
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        ]
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    //response gives us too much stuff that we dont need, we
+    //want the body, but we want it in a format that javascript can read and that is in the format of json
+    //once that promise is complete, yhe information you fetched can be named anything
+    //but in this state we are using the term users to easily identify it. 
+    .then(users => users.forEach(user => {
+      if (user.name === "Clementine Bauch" || user.name === "Clementina DuBuque") {
+        admins.push(user)
+      }
+      this.setState({ monsters: admins })
+    }))
+    // .then(admins => this.setState({ monsters: admins}))
+    // .then(users => this.setState({ monsters: users}))
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {this.state.monsters.map(monster => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))
+      }
     </div>
   );
+  }
 }
 
 export default App;
